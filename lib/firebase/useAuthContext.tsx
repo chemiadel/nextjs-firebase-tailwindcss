0a9a9efdf2da781ef0860bd58dd6bb80695294bd
chemiadel <pace3man@gmail.com>
2021-09-17 03:02:18 +0100

    fixing types

--- lib/firebase/useAuthContext.tsx
+++ lib/firebase/useAuthContext.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect, useContext, createContext } from 'react'
 import { getAuth } from "firebase/auth";
 import { onAuthStateChanged, signOut as signout } from "firebase/auth";
 import { setCookie, destroyCookie} from 'nookies'
-import {useRouter} from 'next/router'
 
 type Props = {
   children: React.ReactNode;
@@ -19,17 +18,17 @@ const authUserContext = createContext<UserContext>({
 });
 
 export default function  AuthContextProvider({children} : Props) {
-  const [authUser, setAuthUser] = useState<any | null>(null);
+  const [authUser, setAuthUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
     const auth=getAuth()
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, (user : any) => {
         console.log('auth state', user)
         if (user) {
             setAuthUser(user)
             // Set
-            user.getIdToken().then(( token )=> setCookie(null, 'idToken', token))
+            user.getIdToken().then(( token : string )=> setCookie(null, 'idToken', token))
         
         }
         if (!user) setAuthUser(null)
@@ -45,7 +44,7 @@ export default function  AuthContextProvider({children} : Props) {
 
 export const useAuth = () => useContext(authUserContext);
 
-export const signOut = async (ctx : any) => {
+export const signOut = async () => {
   const auth=getAuth()
   destroyCookie(null, 'idToken')
   await signout(auth)
