904fd827cc8979d172e4b789885d1aab83326919
Adel Chemi <pace3man@gmail.com>
2022-10-16 06:30:43 +0100

    Add: ESlint + Husky

--- pages/private.tsx
+++ pages/private.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useAuth } from "../lib/authContext";
@@ -6,12 +7,12 @@ const Home: NextPage = () => {
   const { user, loading } = useAuth();
 
   if (loading) return <h1>Loading...</h1>;
-
   if (!user) return <h1>U need to login</h1>;
 
   return (
     <>
       <Head>
+        {" "}
         <title>Private</title>
       </Head>
 
